ce62a1dc9dd04a7eca984403f26a69e99ea9ca9f
Norman Khine <khinester@aqoon.local>
2012-05-21 16:03:57 +0100

    works on redis and we have user groups

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 var fs = require( 'fs' );
 
+var groups = {};
+var users = {};
+var parsedUsers = [];
+
 var server = require( 'http' ).createServer( function( req,res ) {
 	var url;
 	if ( req.url === '/'  ) {
@@ -17,8 +21,6 @@ var server = require( 'http' ).createServer( function( req,res ) {
 	res.end( servedFile );
 });
 
-
-
 var nowjs = require("now");
 var	everyone = nowjs.initialize(server);
 
@@ -32,6 +34,7 @@ everyone.now.logStuff = function(message){
 	console.log( message );
 };
 
+/*
 function getUserCount( user, callback ) {
 	var uKey = user + '_count';
 	redisClient.get( uKey, function( err, reply ) {
@@ -45,13 +48,62 @@ function getUserCount( user, callback ) {
 	} );
 }
 
+*/
+
+function dummyReturn( timeStamp, user, cb ) {
+	cb( null,  { user: user, data: 'dummy message\n' } );
+}
+
+everyone.now.announce = function( ) {
+	var self = this;
+	var name;
+	if ( name = self.now.name ) {
+		if ( users[ name ] ) {
+			users[ name ].online = true;
+		} else {
+			users[ name ] = {
+				nowID: self.user.clientId,
+				online: true,
+				groups: []
+			};
+		}
+	} else { 
+		console.log( 'No name provided' );
+	}
+	console.log( 'push name everywhere' );
+	parsedUsers.push(name);
+	everyone.now.pushUser(name);
+	console.dir( users );
+}
+
+function bye( ) {
+	var self = this;
+	var name = self.now.name;
+	if ( users[ name ] ) {
+		users[ name ].online = false;
+	}
+	console.log( 'Bye bye ', self.now.name );
+};
+
+/* nowjs.on( 'disconnect', bye ); */
+everyone.on( 'leave', bye ); /* keep only higher level event */
+
 
 everyone.now.distributeMessage = function( message ) {
 	var self = this;
-	//console.log( message );
-	
-		
 	everyone.now.receiveMessage(self.now.name, message);
 };
-server.listen( 8080 );
 
+everyone.now.getAllUsers = function( ) {
+	console.log( 'set all ' );
+	everyone.now.setAllUsers( parsedUsers );
+};
+
+everyone.now.nextChunk = function( cursor ) {
+	var self = this;
+	dummyReturn( cursor, this.now.name , function( err, chunk ) {
+		self.now.appendContent( chunk.data );
+	});
+};
+
+server.listen( 8080 );
